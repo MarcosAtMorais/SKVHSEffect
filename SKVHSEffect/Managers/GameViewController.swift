de45f31cb3dbdd12d1958b97c25e46047358ba8b
Marcos Morais <marcostmorais@gmail.com>
2019-01-28 17:02:49 -0200

    Finished shader logic.

--- SKVHSEffect/Managers/GameViewController.swift
+++ SKVHSEffect/Managers/GameViewController.swift
@@ -17,18 +17,15 @@ class GameViewController: UIViewController {
         
         if let view = self.view as! SKView? {
             // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
+            let scene = GameScene(size: view.frame.size)
+            // Set the scale mode to scale to fit the window
+            scene.scaleMode = .aspectFill
             
+            // Present the scene
+            view.presentScene(scene)
             view.ignoresSiblingOrder = true
-            
-            view.showsFPS = true
-            view.showsNodeCount = true
+            view.showsFPS = false
+            view.showsNodeCount = false
         }
     }
 
--- SKVHSEffect/New Group/GameScene.swift
+++ SKVHSEffect/New Group/GameScene.swift
@@ -10,15 +10,31 @@ import SpriteKit
 
 class GameScene: SKScene {
     
-    unowned var background: SKSpriteNode {
-        return SKSpriteNode(imageNamed: "Mockup.png")
-    }
+    let background: SKSpriteNode!
+    
+    let vhsShader: SKShader?
     
     override func didMove(to view: SKView) {
         self.background.position = CGPoint(x: view.frame.midX, y: view.frame.midY)
-        self.background.setScale(2.0)
-        
         self.addChild(self.background)
+        
+        guard let shader = self.vhsShader else {
+            return
+        }
+        self.background.shader = shader
+    }
+    
+    override init(size: CGSize) {
+        self.background = SKSpriteNode(imageNamed: "Mockup.png")
+        self.vhsShader = SKShader(fileNamed: "VHS_Shader.fsh")
+        super.init(size: size)
+        
+        self.vhsShader?.uniforms = [SKUniform(name: "iResolution", vectorFloat2: vector_float2(Float(self.frame.size.width), Float(self.frame.size.height))),
+                                    SKUniform(name: "iChannel0", texture: background.texture)]
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
     
 }
