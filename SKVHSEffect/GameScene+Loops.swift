d35a414d6541c6954184ebd1ad0db5cf4f414043
Marcos Morais <marcostmorais@gmail.com>
2019-01-28 16:13:58 -0200

    Added modules and new file structure.

---
+++ SKVHSEffect/GameScene+Loops.swift
@@ -0,0 +1,15 @@
+//
+//  GameScene+Loops.swift
+//  SKVHSEffect
+//
+//  Created by Marcos Morais on 28/01/19.
+//  Copyright © 2019 Marcos Morais. All rights reserved.
+//
+
+import Foundation
+
+extension GameScene {
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+}
---
+++ SKVHSEffect/GameScene+TouchEvents.swift
@@ -0,0 +1,26 @@
+//
+//  GameScene + TouchEvents.swift
+//  SKVHSEffect
+//
+//  Created by Marcos Morais on 28/01/19.
+//  Copyright © 2019 Marcos Morais. All rights reserved.
+//
+
+/**
+ 
+ Touch Event methods
+ 
+ */
+
+extension GameScene {
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+    }
+    
+}
